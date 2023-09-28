79abeced51188d421f216cae300e0ff010f5b1ff
PogiAbel <pogonyiabel@gmail.com>
2023-09-28 12:24:52 +0200

    document function and rewrite to be independent

--- js/main.js
+++ js/main.js
@@ -1,27 +1,42 @@
-const mainElement = document.getElementById("Fancy");
+
 const textCircle = document.getElementsByClassName("text")[0];
-const cursor = document.getElementsByClassName("cursor")[0];
 const overlay = document.getElementsByClassName("overlay")[0];
+const overlay2 = document.getElementsByClassName("overlay")[1];
 const alphabet = [...'abcdefghijklmnopqrstuvwxyz1234567890'];
-var root = document.documentElement;
+
+/**
+ * 
+ * @returns {number} random number between 0 and 35 for alphabet array
+ */
 var generateInt = function(){
     return Math.floor(Math.random()*alphabet.length);
 }
-var generateText = function(e){
+/**
+ * 
+ * @param {HTMLElement} div containning the text 
+ */
+var generateText = function(div){
     let text = '';
     for(let i=0;i<4000;i++){
         const num = generateInt();
         text += alphabet[num];
     }
-    textCircle.textContent = text;
+    div.textContent = text;
 }
+/**
+ * 
+ * @param {MouseEvent} e mouseEvent
+ */
 var mouseMovement = function(e){
-    const x = e.clientX;
-    const y = e.clientY;
+    const x = e.offsetX;
+    const y = e.offsetY;
+    const parentDiv = e.target.parentElement;
+    const cursor = parentDiv.querySelector('.cursor');
     cursor.style.marginLeft = (x-100) + 'px';
     cursor.style.marginTop = (y-100) + 'px';
-    generateText();
+    generateText(parentDiv.querySelector('.text'));
 }
-generateText();
+generateText(textCircle);
 
 overlay.addEventListener('mousemove',mouseMovement, false)
+overlay2.addEventListener('mousemove',mouseMovement, false)
